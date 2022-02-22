fc8ddafe6f8d5ce076f8b045c75907802f27381f
sami92news <sami.akram@92newshd.tv>
2022-02-22 13:06:39 +0500

    first commit

---
+++ pos_inline_customer/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pos Inline Customer",
+    'description': """
+        Customer Selection Brought to Order Page
+    """,
+
+    'author': "sami",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['point_of_sale'],
+
+    # always loaded
+    'data': [
+    ],
+    'qweb': [
+        'static/inline_customer.xml',
+    ],
+    'application': True
+}
